19a922fbbee53c9675eb1b8e5a1fd09a639927f4
Jagruthi Peyyala <60375090+jagruthi816@users.noreply.github.com>
2025-01-24 10:07:22 +0530

    Update Frog Jump.java

--- Dynamic Programming/1D DP/Frog Jump.java
+++ Dynamic Programming/1D DP/Frog Jump.java
@@ -81,4 +81,25 @@ class Solution {
     }
 }
 
-  
+/* Method 3 
+  Tabulation */
+class Solution {
+    public int frogJump(int[] heights) {
+        int n = heights.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        // Base case
+        dp[0] = 0; 
+
+        // Iterative calculation
+        for (int ind = 1; ind < n; ind++) {
+            int jumpOne = dp[ind - 1] + Math.abs(heights[ind] - heights[ind - 1]);
+            int jumpTwo = Integer.MAX_VALUE;
+            if (ind > 1)
+            // Store minimum energy for this stair
+                jumpTwo = dp[ind - 2] + Math.abs(heights[ind] - heights[ind - 2]);
+            dp[ind] = Math.min(jumpOne, jumpTwo); 
+        }
+        // Return the minimum energy required to reach the last stair
+        return dp[n - 1]; 
+    }
